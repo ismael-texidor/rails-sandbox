6138cbae2c8c6315977cc81096eb14df0fe0a0a5
Ismael Texidor-Rodriguez <ismael.texidor@outlook.com>
2020-09-03 19:37:44 -0400

    lets add rspec to the mix
    Generate command good, but so much boilerplate code! gosh!

--- db/migrate/20200903233050_create_sandboxes.rb
+++ db/migrate/20200903233050_create_sandboxes.rb
@@ -1,8 +1,6 @@
 class CreateSandboxes < ActiveRecord::Migration[6.0]
   def change
     create_table :sandboxes do |t|
-      t.integer :sand_grains
-      t.string :box_name
 
       t.timestamps
     end
